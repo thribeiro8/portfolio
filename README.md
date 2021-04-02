ed0d4e7828f1c08be9b88499e25f3fea4ee3a84b
Thomas Ribeiro <thomas18rs@gmail.com>
2021-04-01 21:41:42 -0300

    :package: budget calculation

--- README.md
+++ README.md
@@ -15,8 +15,19 @@ O controle das tarefas neste projeto será realizado no Github.
 
 ### Extensões
 
-- [x] Auto Close Tag
-- [x] Gitlens
-- [x] Highligth Match Tag
-- [x] Live Server
-- [x] VS Color Picker
+- [x] Auto Close Tag;
+- [x] Gitlens;
+- [x] Highligth Match Tag;
+- [x] Live Server;
+- [x] VS Color Picker.
+
+## Regras do negócio
+
+- [] Valor por página: R$ 100;
+- [] Script: 10% do custo das páginas;
+- [] Layout: R$ 500,00;
+- [] Taxa de Urgência: 10% do custo total por semana de urgência.
+
+## Colaborações
+
+Caso queira colaborar com ideias e sugestões, fique à vontade para me contatar.
--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 document.querySelector('.hamburguer').addEventListener('click', () => {
   document.querySelector('.container').classList.toggle('show-menu');
 });
+
+document.querySelector('#qtd').addEventListener('change', updatePrice);
+document.querySelector('#js').addEventListener('change', updatePrice);
+document.querySelector('#layout-sim').addEventListener('change', updatePrice);
+document.querySelector('#layout-nao').addEventListener('change', updatePrice);
+document.querySelector('#prazo').addEventListener('change', () => {
+  const prazo = document.querySelector('#prazo').value;
+  // prettier-ignore
+  document.querySelector('label[for=prazo]').innerHTML = `Prazo: ${prazo} semanas`;
+  updatePrice();
+});
+
+function updatePrice() {
+  const qtd = document.querySelector('#qtd').value;
+  const temJS = document.querySelector('#js').checked;
+  const incluiLayout = document.querySelector('#layout-sim').checked;
+  const prazo = document.querySelector('#prazo').value;
+
+  let preco = qtd * 100;
+  if (temJS) preco *= 1.1;
+  if (incluiLayout) preco += 500;
+  let taxaUrgencia = 1 - prazo * 0.1;
+  preco *= 1 + taxaUrgencia;
+
+  document.querySelector('#preco').innerHTML = `R$ ${preco.toFixed(2)}`;
+}
